4d6819699a54f2bfadfcea917d8195bb628a2167
idowu-aminat <meenah3710213@gmail.com>
2025-05-31 14:52:58 +0100

    Add files via upload

---
+++ fashion/script.js
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Change Text With Javascript</title>
+    </head>
+</html>
+<body>
+    <h1 id="greeting">HELLO</h1>
+<button onclick="changeText()">
+
+    <script>
+        function changeText() {document.getElementById {'greeting'}.InnerHTML="You Clicked the button!"}
+    </script>
+</button>
+</body>
